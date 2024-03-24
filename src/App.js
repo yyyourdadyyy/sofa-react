cc4ee04a00e7d71c60480f8bf15ffce3e4de9a2a
yyyourdadyyy <repeteevkiril@gmail.com>
2024-03-24 16:06:45 +0500

    Files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from "./components/header";
+import Footer from "./components/footer";
+
+function App() {
+  return (
+    <div class="wrapper">
+      <Header />
+      <Footer />
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/components/header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+         <div class = "logo"></div>
+         <div className="present">
+          <div class="description">
+            <div class="present-logo">The best sofas for your home</div>
+            <div class="text">
+            at low prices
+            </div>
+            </div>
+            <div className="present-img"></div>
+         </div>
+    </header>
+    
+  )
+}
